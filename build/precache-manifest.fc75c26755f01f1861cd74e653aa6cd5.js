013519f4f70b6652a497d6602039f3c09c30d83a
liuzhibin-cn <riccc.cn@gmail.com>
2018-12-04 11:50:03 +0800

    from research

---
+++ build/precache-manifest.fc75c26755f01f1861cd74e653aa6cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4204095f0815fdb5c149",
+    "url": "/react-js/build/static/js/runtime~main.4204095f.js"
+  },
+  {
+    "revision": "7cb6244d2cde0da37b92",
+    "url": "/react-js/build/static/js/main.7cb6244d.chunk.js"
+  },
+  {
+    "revision": "b9e8abfadc501ccbf3ed",
+    "url": "/react-js/build/static/js/1.b9e8abfa.chunk.js"
+  },
+  {
+    "revision": "7cb6244d2cde0da37b92",
+    "url": "/react-js/build/static/css/main.f0e135f3.chunk.css"
+  },
+  {
+    "revision": "b9e8abfadc501ccbf3ed",
+    "url": "/react-js/build/static/css/1.2228872c.chunk.css"
+  },
+  {
+    "revision": "87df4df8eaf9add94a214359fd0c8977",
+    "url": "/react-js/build/index.html"
+  }
+];
